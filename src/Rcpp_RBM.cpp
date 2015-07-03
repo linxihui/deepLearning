118572f067bcfef4801991cc7e53abcae3af6cf1
linxihui <xihuil.silence@gmail.com>
2015-07-03 00:37:45 -0400

    Rcpp interface of RBM

---
+++ src/Rcpp_RBM.cpp
@@ -0,0 +1,24 @@
+List C_RBM(MatrixXd x, int hidden, int numepochs = 3, int batchsize = 100, 
+	double learning_rate = 0.8, double learning_rate_scale = 1, int cd = 1,
+	double momentum = 0.5
+	) {
+	RBM rbm(xx.cols(), hidden);
+	VectorXd error = rbm.train(x, numepochs, batchsize, learning_rate, momentum, learning_rate_scale, cd);
+	return List::create(
+		Named("rbm") = wrap(rbm),
+		Named("error") = error
+		);
+	}
+
+
+List C_RBM_train_more(RBM rbm, Matrix x, double learning_rate, 
+		double learning_rate_scale, double momentum,
+		string hidden_type, string visible_type, int cd
+	) {
+	//RBM rbm(rbm_trained);
+	VectorXd error = rbm.train(x, numepochs, batchsize, learning_rate, momentum, learning_rate_scale, cd);
+	return List::create(
+		Named("rbm") = wrap(rbm),
+		Named("error") = error
+		);
+	}
