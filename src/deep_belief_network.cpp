d98d91d8a7ae3658885d15cbd9dfec29b09cda6a
linxihui <xihuil.silence@gmail.com>
2015-10-21 23:15:01 -0400

    fixed bugs; finished DBN;

--- src/deep_belief_network.cpp
+++ src/deep_belief_network.cpp
@@ -1,3 +1,5 @@
+#include "deepnet.hpp"
+
 DBN::DBN(int in_dim, VectorXi hid) {
 	input_dim = in_dim;
 	hidden = hid;
@@ -6,35 +8,43 @@ DBN::DBN(int in_dim, VectorXi hid) {
 	vis_hid.segment(1, hidden.size()) = hidden;
 	layer.reserve(hidden.size());
 	for (int i = 0; i < hidden.size(); i++) {
-		layer.push_back(rbm(vis_hid[i], vis_hid[i+1]));
+		layer.push_back(RBM(vis_hid[i], vis_hid[i+1]));
 		}
 	}
 
 
-MatrixXd DNB::train(MatrixXd &x, double learning_rate, double learning_rate_scale, 
+MatrixXd DBN::train(MatrixXd &x, double learning_rate, double learning_rate_scale, 
 	double momentum, int numepochs, int batchsize, int CD, bool verbose) {
 	MatrixXd pre_layer(x);
 	for (int i = 0; i < hidden.size(); i++) {
 		if (verbose) cout << "Training layer " << i+1 << "..." << endl;
 		layer[i].train(pre_layer, numepochs, batchsize, learning_rate, momentum, learning_rate_scale, CD);
-		pre_layer = layer[i].prob_h_given_v(pre_layer)
-		}
-	return pre_layer;
-	}
-
-
-MatrixXd DBN::extractFeature(MatrixXd v) {
-	MatrixXd pre_layer(v);
-	for (int i = 0; i < hidden.size(); i++) {
 		pre_layer = layer[i].prob_h_given_v(pre_layer);
 		}
 	return pre_layer;
 	}
 
-MatrixXd DBN::reconstruct(MatrixXd h) {
-	MatrixXd pre_layer(h);
-	for (int i = hidden.size()-1; i >=0; i--) {
-		pre_layer = layer[i].prob_v_given_h(h);
+
+MatrixXd DBN::extractHiddenFeature(MatrixXd v) {
+	MatrixXd pre_layer(v);
+	for (int i = 0; i < hidden.size(); i++) {
+		cout << "i = " << i << endl;
+		pre_layer = layer[i].prob_h_given_v(pre_layer);
 		}
 	return pre_layer;
+	}
+
+MatrixXd DBN::generateFromHidden(MatrixXd h) {
+	MatrixXd pre_layer(h);
+	for (int i = hidden.size()-1; i >= 0; i--) {
+		cout << "i = " << i << endl;
+		pre_layer = layer[i].prob_v_given_h(pre_layer);
+		}
+	cout << "Done" << endl;
+	return pre_layer;
+	}
+
+MatrixXd DBN::reconstructFromInput(MatrixXd x) {
+	MatrixXd pre_layer = extractHiddenFeature(x);
+	return generateFromHidden(pre_layer);
 	}
